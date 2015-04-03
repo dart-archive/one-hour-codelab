347972ea87c785c06c30afcbc64b3fbcf97d4057
Sharon Zakhour <shaza@google.com>
2015-04-03 16:42:18 -0700

    Moving files around to match the codelab text.

---
+++ 2-1-simple/lib/server/piratesapi.dart
@@ -0,0 +1,41 @@
+// Copyright (c) 2015, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+library pirate.server;
+
+import 'dart:convert' show JSON;
+import 'dart:io';
+import 'package:rpc/rpc.dart';
+
+import '../common/messages.dart';
+import '../common/utils.dart';
+
+@ApiClass(version: 'v1')
+class PiratesApi {
+  final Map<int, Pirate> _alivePirates = {};
+  PirateShanghaier _shanghaier;
+  Map<String, List<String>> _properPirates;
+
+  PiratesApi() {
+    var namesFile =
+        new File('packages/sample/server/piratenames.json');
+    _properPirates = JSON.decode(namesFile.readAsStringSync());
+    _shanghaier = new PirateShanghaier(_properPirates);
+  }
+
+  @ApiMethod(path: 'pirates')
+  List<Pirate> listPirates() {
+    return _alivePirates.values.toList();
+  }
+
+  @ApiMethod(path: 'shanghai')
+  Pirate shanghaiAPirate() {
+    var pirate = _shanghaier.shanghaiAPirate();
+    if (pirate == null) {
+      throw new InternalServerError('Ran out of pirates!');
+    }
+    _alivePirates[pirate.hashCode] = pirate;
+    return pirate;
+  }
+}
