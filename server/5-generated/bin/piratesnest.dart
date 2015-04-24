56b283468a7c5f203b294e4715d70f155c7f2eb4
Gustav Wibling <wibling@google.com>
2015-04-24 10:54:57 +0200

    Minor tweaks to the code.

--- server/5-generated/bin/piratesnest.dart
+++ server/5-generated/bin/piratesnest.dart
@@ -15,13 +15,14 @@ final ApiServer _apiServer = new ApiServer(prettyPrint: true);
 
 main() async {
   // Add a bit of logging...
-  Logger.root.level = Level.INFO;
-  Logger.root.onRecord.listen(print);
+  Logger.root..level = Level.INFO
+             ..onRecord.listen(print);
 
   // Set up a server serving the pirate API.
   _apiServer.addApi(new PiratesApi());
   HttpServer server =
       await HttpServer.bind(InternetAddress.ANY_IP_V4, 8088);
   server.listen(_apiServer.httpRequestHandler);
-  print('Server listening on http://${server.address.address}:${server.port}');
+  print('Server listening on http://${server.address.host}:'
+        '${server.port}');
 }
